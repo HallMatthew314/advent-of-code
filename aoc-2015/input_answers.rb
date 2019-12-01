7311ea6da1c726f040dc5fd1fa4f5d20eb96bdbd
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-02 11:38:10 +1300

    Input file can be specified as an argument

--- aoc-2015/input_answers.rb
+++ aoc-2015/input_answers.rb
@@ -3,28 +3,28 @@
 
 require_relative "aoc_2015_ruby.rb"
 
-def day1
-  directions = File.read("day1_input.txt").strip
+def day1(path)
+  directions = File.read(path || "day1_input.txt").strip
   puts "Part 1: #{AOC2015.day1_part1(directions)}"
   puts "Part 2: #{AOC2015.day1_part2(directions)}"
 end
 
-def day2
-  dimensions = File.readlines("day2_input.txt").map { |l| l.strip }
+def day2(path)
+  dimensions = File.readlines(path || "day2_input.txt").map { |l| l.strip }
   puts "Part 1: #{AOC2015.day2_part1(dimensions)}"
   puts "Part 2: #{AOC2015.day2_part2(dimensions)}"
 end
 
 def day3
-  directions = File.read("day3_input.txt").strip
+  directions = File.read(path || "day3_input.txt").strip
   puts "Part 1: #{AOC2015.day3_part1(directions)}"
   puts "Part 2: #{AOC2015.day3_part2(directions)}"
 end
 
 case ARGV[0].to_i
-when 1 then day1
-when 2 then day2
-when 3 then day3
-else puts "Please supply a day to run: ./input_answers.rb <DAY NUMBER>"
+when 1 then day1(ARGV[1])
+when 2 then day2(ARGV[1])
+when 3 then day3(ARGV[1])
+else puts "Please supply a day to run: ./input_answers.rb <DAY NUMBER> (INPUT PATH)"
 end
 
