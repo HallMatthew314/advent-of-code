c1e49a7444e3f6e8a022d2644a34583da18a7f83
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-01 17:47:45 +1300

    Prep for 2019

---
+++ aoc-2019/aoc_2019_ruby.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+module AOC2019
+
+  module_function
+
+  def day1_part1
+    # soon
+    raise NotImplementedError
+  end
+end
+
---
+++ aoc-2019/tc_aoc_2019_ruby.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "test/unit"
+require_relative "aoc_2019_ruby.rb"
+
+class TestAOC2019 < Test::Unit::TestCase
+
+  def test_day1_part1
+    assert(true)
+  end
+end
+
