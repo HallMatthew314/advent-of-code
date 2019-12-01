6c44fad9c615b1786c0ad9916f87106876f06ef3
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-02 11:39:15 +1300

    Rename 'mods' to 'masses'

--- aoc-2019/aoc_2019_ruby.rb
+++ aoc-2019/aoc_2019_ruby.rb
@@ -4,17 +4,17 @@ module AOC2019
 
   module_function
 
-  def day1_part1(mods)
-    mods.map { |m| m.to_i / 3 - 2 }.sum
+  def day1_part1(masses)
+    masses.map { |m| m.to_i / 3 - 2 }.sum
   end
 
-  def day1_part2(mods)
+  def day1_part2(masses)
     fuel = ->(m) do
       f = m / 3 - 2
       f > 0 ? f + fuel.call(f) : 0
     end
 
-    mods.map { |m| fuel.call(m.to_i) }.sum
+    masses.map { |m| fuel.call(m.to_i) }.sum
   end
 end
 
