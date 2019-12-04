83bf38f47ea992ee411ce980feb336c0de7a2672
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-04 15:37:02 +1300

    Tests for 3-2

--- aoc-2019/tc_aoc_2019_ruby.rb
+++ aoc-2019/tc_aoc_2019_ruby.rb
@@ -35,5 +35,20 @@ class TestAOC2019 < Test::Unit::TestCase
       "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(",")
     ]))
   end
+
+  def test_day3_part2
+    assert_equal(30, AOC2019.day3_part2([
+      "R8,U5,L5,D3".split(","),
+      "U7,R6,D4,L4".split(",")
+    ]))
+    assert_equal(610, AOC2019.day3_part2([
+      "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(","),
+      "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
+    ]))
+    assert_equal(410, AOC2019.day3_part2([
+      "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(","),
+      "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(",")
+    ]))
+  end
 end
 
