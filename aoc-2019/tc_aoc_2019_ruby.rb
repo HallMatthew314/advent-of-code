6241feb14c1a443141e9139c16a91de6a8ab46bb
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-04 15:30:57 +1300

    Day 3 unit tests

--- aoc-2019/tc_aoc_2019_ruby.rb
+++ aoc-2019/tc_aoc_2019_ruby.rb
@@ -20,5 +20,20 @@ class TestAOC2019 < Test::Unit::TestCase
   end
 
   # No examples to test for Day 2
+
+  def test_day3_part1
+    assert_equal(6, AOC2019.day3_part1([
+      "R8,U5,L5,D3".split(","),
+      "U7,R6,D4,L4".split(",")
+    ]))
+    assert_equal(159, AOC2019.day3_part1([
+      "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(","),
+      "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
+    ]))
+    assert_equal(135, AOC2019.day3_part1([
+      "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(","),
+      "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(",")
+    ]))
+  end
 end
 
