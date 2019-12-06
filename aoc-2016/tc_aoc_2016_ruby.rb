6770ae01b2ff709cf67e763f352b944d179fbda2
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-06 17:31:07 +1300

    Unit tests

--- aoc-2016/tc_aoc_2016_ruby.rb
+++ aoc-2016/tc_aoc_2016_ruby.rb
@@ -15,5 +15,23 @@ class TestAOC2016 < Test::Unit::TestCase
   def test_day1_part2
     assert_equal(4, AOC2016.day1_part2("R8, R4, R4, R8".split(", ")))
   end
+
+  def test_day2_part1
+    assert_equal("1985", AOC2016.day2_part1([
+      "ULL",
+      "RRDDD",
+      "LURDL",
+      "UUUUD"
+    ]))
+  end
+
+  def test_day2_part2
+    assert_equal("5DB3", AOC2016.day2_part2([
+      "ULL",
+      "RRDDD",
+      "LURDL",
+      "UUUUD"
+    ]))
+  end
 end
 
