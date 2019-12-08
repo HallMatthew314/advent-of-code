02b0a285bdbc072a1abb310e721c57909b6f7feb
Matthew Hall <hallmg1@student.op.ac.nz>
2019-12-08 17:10:57 +1300

    Refactor intcode computer class

--- aoc-2019/intcode_computer5.rb
+++ aoc-2019/intcode_computer5.rb
@@ -3,87 +3,147 @@
 # Intcode Computer class following specifications of Day 5.
 class IntcodeComputer5
 
+  # @state values:
+  # READY - Program has not started.
+  # RUNNING - Program is running.
+  # IDLE - Program is waiting for input (queue is empty).
+  # CRASH - Program encountered invalid opcode.
+  # DONE - Program reached halt opcode.
+
   def initialize(program)
-    @program = program
-    unless @program.is_a?(Array)
+    unless program.is_a?(Array)
       raise ArgumentError, "Program was not an Array"
     end
+
+    @program = program.dup
+
+    reset
   end
 
-  def run(input_queue=[])
-    raise ArgumentError, "Input was not an Array" unless input_queue.is_a?(Array)
+  def reset
+    @memory = @program.dup
+    @cycle = 0
+    @mem_ptr = 0
+    @state = "READY"
+    @input_queue = []
+    @output_queue = []
+    @error_log = nil
+  end
 
-    code = @program.dup
-    outputs = []
-    inst_ptr = 0
-    cycle = 0
+  def fetch_output
+    @output_queue.shift
+  end
 
-    loop do
-      cycle += 1
-      # HLT
-      return outputs if code[inst_ptr] % 100 == 99
+  # TODO: Allow send Arrays of integers
+  def send_input(i)
+    raise ArgumentError, "Inputs must be integers" unless i.is_a?(Integer)
+    @input_queue << i
+  end
 
-      # Decode
-      # Parameter 1
-      p1 = if (code[inst_ptr] / 100) % 10 == 1
-             code[inst_ptr + 1]
-           else
-             code[code[inst_ptr + 1]]
-           end
-      # Parameter 2
-      p2 = if (code[inst_ptr] / 1_000) % 10 == 1
-             code[inst_ptr + 2]
-           else
-             code[code[inst_ptr + 2]]
-           end
-      # Parameter 3
-      p3 = if code[inst_ptr] / 10_000 == 1
-             code[inst_ptr + 3]
-           else
-             code[code[inst_ptr + 3]]
-           end
-      # Opcode
-      opcode = code[inst_ptr] % 100
+  def run(inputs=[])
+    raise ArgumentError, "Input was not an Array" unless inputs.is_a?(Array)
+    inputs.each { |i| send_input(i) }
 
-      # Execute
-      case opcode
-      # ADD
-      when 1
-        code[code[inst_ptr + 3]] = p1 + p2
-        inst_ptr += 4
-      # MUL
-      when 2
-        code[code[inst_ptr + 3]] = p1 * p2
-        inst_ptr += 4
-      # INP
-      when 3
-        raise "Ran out of inputs" if input_queue.empty?
-        code[code[inst_ptr + 1]] = input_queue.shift
-        inst_ptr += 2
-      # OUT
-      when 4
-        outputs << code[code[inst_ptr + 1]]
-        inst_ptr += 2
-      # JEZ
-      when 5
-        inst_ptr = p1 == 0 ? inst_ptr + 3 : p2
-      # JNZ
-      when 6
-        inst_ptr = p1 == 0 ? p2 : inst_ptr + 3
-      # LST
-      when 7
-        code[code[inst_ptr + 3]] = p1 < p2 ? 1 :0
-        inst_ptr += 4
-      # EQL
-      when 8
-        code[code[inst_ptr + 3]] = p1 == p2 ? 1 : 0
-        inst_ptr += 4
+    @state = "RUNNING"
+    step while @state == "RUNNING"
 
-      else 
-        raise "Invalid opcode: #{opcode} - " \
-              "INST_PTR: #{inst_ptr}:#{code[inst_ptr]} - " \
-              "CYCLE #{cycle}"
+    return @state
+  end
+
+  def step
+    @cycle += 1
+
+    # HLT
+    if @memory[@mem_ptr] % 100 == 99
+      @state = "DONE"
+      return
+    end
+
+    # Decode
+    # Parameter 1
+    p1 = if (@memory[@mem_ptr] / 100) % 10 == 1
+           @memory[@mem_ptr + 1]
+         else
+           @memory[@memory[@mem_ptr + 1]]
+         end
+
+    # Parameter 2
+    p2 = if (@memory[@mem_ptr] / 1_000) % 10 == 1
+           @memory[@mem_ptr + 2]
+         else
+           @memory[@memory[@mem_ptr + 2]]
+         end
+
+    # Parameter 3
+    p3 = if @memory[@mem_ptr] / 10_000 == 1
+            @memory[@mem_ptr + 3]
+         else
+            @memory[@memory[@mem_ptr + 3]]
+         end
+
+    # Opcode
+    opcode = @memory[@mem_ptr] % 100
+
+    # Execute
+    case opcode
+
+    # ADD
+    when 1
+      @memory[@memory[@mem_ptr + 3]] = p1 + p2
+      @mem_ptr += 4
+
+    # MUL
+    when 2
+      @memory[@memory[@mem_ptr + 3]] = p1 * p2
+      @mem_ptr += 4
+
+    # INP
+    when 3
+      if @input_queue.empty?
+        @state = "IDLE"
+      else
+        @memory[@memory[@mem_ptr + 1]] = @input_queue.shift
+        @mem_ptr += 2
+        @state = "RUNNING"
       end
+
+    # OUT
+    when 4
+      @output_queue << @memory[@memory[@mem_ptr + 1]]
+      @mem_ptr += 2
+
+    # JEZ
+    when 5
+      @mem_ptr = p1 == 0 ? @mem_ptr + 3 : p2
+
+    # JNZ
+    when 6
+      @mem_ptr = p1 == 0 ? p2 : @mem_ptr + 3
+
+    # LST
+    when 7
+      @memory[@memory[@mem_ptr + 3]] = p1 < p2 ? 1 :0
+      @mem_ptr += 4
+
+    # EQL
+    when 8
+      @memory[@memory[@mem_ptr + 3]] = p1 == p2 ? 1 : 0
+      @mem_ptr += 4
+
+    else
+      @state = "CRASH"
+      @error_log = "Invalid opcode: #{opcode} - " \
+        "INST_PTR: #{@mem_ptr}:#{@memory[@mem_ptr]} - " \
+        "CYCLE #{cycle}"
     end
   end
+
+  def state
+    @state
+  end
+
+  def error_log
+    @error_log
+  end
 end
+
--- aoc-2019/tc_intcode_computer5.rb
+++ aoc-2019/tc_intcode_computer5.rb
@@ -7,10 +7,32 @@ require_relative "intcode_computer5.rb"
 class TestIntcodeComputer5 < Test::Unit::TestCase
 
   def test_day5_part2
-    #code = File.read("day5_input.txt").strip.split(",").map { |o| o.to_i }
     code = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
     com = IntcodeComputer5.new(code)
-    assert_equal(1, com.run([8]).first)
+    com.send_input(8)
+    com.run
+    assert_equal(1, com.fetch_output)
+  end
+
+  def test_idle_state
+    code = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+    com = IntcodeComputer5.new(code)
+    com.run
+    assert_equal("IDLE", com.state)
+
+    com.step
+    assert_equal("IDLE", com.state)
+
+    com.run
+    assert_equal("IDLE", com.state)
+
+    com.send_input(8)
+    com.step
+    assert_equal("RUNNING", com.state)
+
+    com.run
+    assert_equal("DONE", com.state)
+    assert_equal(1, com.fetch_output)
   end
 end
 
