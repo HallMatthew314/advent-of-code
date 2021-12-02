0debae942fbd18b9117477f6bb5e22523049bfb3
Matthew Hall <hallmatthew314@gmail.com>
2021-12-02 19:07:03 +1300

    day 2

--- Haskell/AOC2021/AOC2021.hs
+++ Haskell/AOC2021/AOC2021.hs
@@ -12,11 +12,35 @@ day1Part2 = day1Template (g . f)
     g xs = zipWith (-) xs $ tail xs
     f xs = zipWith3 (\ x y z -> x + y + z) xs (tail xs) (drop 2 xs)
 
+day2Part1 :: String -> Int
+day2Part1 = (\ (x, y) -> x * y) . foldl f (0, 0) . parseDirections
+  where
+    f t d = d t
+    parseDirections = go . words
+      where
+        go [] = []
+        go ("forward":n:xs) = (\ (x, y) -> (x + read n, y)):go xs
+        go ("down":n:xs)    = (\ (x, y) -> (x, y + read n)):go xs
+        go ("up":n:xs)      = (\ (x, y) -> (x, y - read n)):go xs
+
+day2Part2 :: String -> Int
+day2Part2 = (\ (x, y, _) -> x * y) . foldl f (0, 0, 0) . parseDirections
+  where
+    f t d = d t
+    parseDirections = go . words
+      where
+        go [] = []
+        go ("down":n:xs)    = (\ (x, y, a) -> (x, y, a + read n)):go xs
+        go ("up":n:xs)      = (\ (x, y, a) -> (x, y, a - read n)):go xs
+        go ("forward":n:xs) = (\ (x, y, a) -> (x + read n, y + a * read n, a)):go xs
+
 run :: Show a => (String -> a) -> FilePath -> IO ()
 run day path = readFile path >>= (putStrLn . show . day)
 
 runD1P1 = run day1Part1 "day1_input.txt"
 runD1P2 = run day1Part2 "day1_input.txt"
+runD2P1 = run day2Part1 "day2_input.txt"
+runD2P2 = run day2Part2 "day2_input.txt"
 
 main :: IO ()
 main = putStrLn "hi"
