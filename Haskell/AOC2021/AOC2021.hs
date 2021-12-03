285c1ff1e02f0026d1245fd23e871e9716aab2d4
Matthew Hall <hallmatthew314@gmail.com>
2021-12-03 23:40:56 +1300

    Day 3 part 1

--- Haskell/AOC2021/AOC2021.hs
+++ Haskell/AOC2021/AOC2021.hs
@@ -1,5 +1,10 @@
 module AOC2021 where
 
+import Data.Char (digitToInt)
+
+binStrToInt :: String -> Int
+binStrToInt = sum . zipWith (\ e m -> 2^e * m) [0 ..] . map digitToInt . reverse
+
 day1Template :: ([Int] -> [Int]) -> String -> Int
 day1Template go = length . filter (<0) . go . map read . words
 
@@ -34,6 +39,21 @@ day2Part2 = (\ (x, y, _) -> x * y) . foldl f (0, 0, 0) . parseDirections
         go ("up":n:xs)      = (\ (x, y, a) -> (x, y, a - read n)):go xs
         go ("forward":n:xs) = (\ (x, y, a) -> (x + read n, y + a * read n, a)):go xs
 
+day3Part1 :: String -> Int
+day3Part1 = (\ (g, e) -> g * e) . d3GammaEpsilon
+
+d3GammaEpsilon :: String -> (Int, Int)
+d3GammaEpsilon = f3 . foldr f2 ([],[]) . foldr f1 (repeat 0) . words
+  where
+    f1 cs xs = zipWith (\ c x -> (if c == '1' then succ else pred) x) cs xs
+    f2 n (g, e)
+      | n > 0     = ('1':g, '0':e)
+      | n < 0     = ('0':g, '1':e)
+      | otherwise = error "no most-common bit"
+    f3 (g, e) = (binStrToInt g, binStrToInt e)
+
+d3test = "00100 11110 10110 10111 10101 01111 00111 11100 10000 11001 00010 01010"
+
 run :: Show a => (String -> a) -> FilePath -> IO ()
 run day path = readFile path >>= (putStrLn . show . day)
 
@@ -41,6 +61,7 @@ runD1P1 = run day1Part1 "day1_input.txt"
 runD1P2 = run day1Part2 "day1_input.txt"
 runD2P1 = run day2Part1 "day2_input.txt"
 runD2P2 = run day2Part2 "day2_input.txt"
+runD3P1 = run day3Part1 "day3_input.txt"
 
 main :: IO ()
 main = putStrLn "hi"
