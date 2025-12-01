7e4b927ba413b8a3ab11c487b1bede3e1651be6e
Matthew Hall <matthew@matthewhall.xyz>
2025-12-02 12:24:37 +1300

    Better but over-engineered solutions for 2025 day 1

--- Rust/aoc2025/src/main.rs
+++ Rust/aoc2025/src/main.rs
@@ -1,95 +1,97 @@
 use std::fs;
 
-enum D1Instruction {
-    Left(u32),
-    Right(u32),
+struct D1DialTurn {
+    turn_right: bool,
+    full_rotations: u32,
+    remaining_rotation: u32,
 }
 
-fn d1_parse_instruction(inp: &str) -> Option<D1Instruction> {
-    let (lr, num_s) = inp.split_at_checked(1)?;
-    let num = u32::from_str_radix(num_s, 10).ok()?;
-    match lr {
-        "L" => Some(D1Instruction::Left(num)),
-        "R" => Some(D1Instruction::Right(num)),
-        _   => None,
+impl D1DialTurn {
+    fn parse(inp: &str) -> Option<Self> {
+        let (lr, num_s) = inp.split_at_checked(1)?;
+        let num = u32::from_str_radix(num_s, 10).ok()?;
+
+        let turn_right = match lr {
+            "L" => false,
+            "R" => true,
+            _   => { return None; },
+        };
+        let full_rotations = num / 100;
+        let remaining_rotation = num % 100;
+
+        Some(Self { turn_right, full_rotations, remaining_rotation })
+    }
+}
+
+struct D1Dial {
+    pointing_at: u32,
+}
+
+impl D1Dial {
+    fn new() -> Self {
+        Self { pointing_at: 50 }
+    }
+
+    fn turn(&mut self, dt: &D1DialTurn) -> u32 {
+        if dt.turn_right {
+            self.pointing_at += dt.remaining_rotation;
+            if self.pointing_at >= 100 {
+                self.pointing_at %= 100;
+                dt.full_rotations + 1
+            } else {
+                dt.full_rotations
+            }
+        } else {
+            // turn left
+            let mut zeroes = dt.full_rotations;
+
+            if self.pointing_at == 0 {
+                self.pointing_at = 100 - dt.remaining_rotation;
+            } else {
+                self.pointing_at += 100;
+                self.pointing_at -= dt.remaining_rotation;
+                if self.pointing_at <= 100 {
+                    zeroes += 1;
+                }
+                self.pointing_at %= 100;
+            }
+            zeroes
+        }
     }
 }
 
 fn day1part1(input: &str) -> u32 {
-    let mut dial: u32 = 50;
+    let mut dial = D1Dial::new();
     let mut zeroes: u32 = 0;
 
     for s in input.split_whitespace() {
-        match d1_parse_instruction(s) {
-            None => panic!("Invalid instruction: {:?}", s),
-            Some(D1Instruction::Left(n)) => {
-                let n = n % 100;
-                if n > dial {
-                    dial += 100;
-                }
-                dial -= n;
-            }
-            Some(D1Instruction::Right(n)) => {
-                let n = n % 100;
-                dial = (dial + n) % 100;
-            }
-        }
-        if dial == 0 {
+        let old = dial.pointing_at;
+        let Some(dt) = D1DialTurn::parse(s) else {
+            panic!("Invalid turn: {}", s);
+        };
+        dial.turn(&dt);
+        if dial.pointing_at == 0 {
             zeroes += 1;
         }
+        println!("{} => {}", old, dial.pointing_at);
     }
 
     zeroes
 }
 
 fn day1part2(input: &str) -> u32 {
-    let mut dial: u32 = 50;
-    let mut clicks: u32 = 0;
+    let mut dial = D1Dial::new();
+    let mut zeroes: u32 = 0;
 
     for s in input.split_whitespace() {
-        println!("dial:{}", dial);
-        match d1_parse_instruction(s) {
-            None => panic!("Invalid instruction"),
-            // much dumber solution for now
-            Some(D1Instruction::Left(mut n)) => {
-                while n >= 100 {
-                    println!("click");
-                    clicks += 1;
-                    n -= 100;
-                }
+        let Some(dt) = D1DialTurn::parse(s) else {
+            panic!("Invalid turn: {}", s);
+        };
+        zeroes += dial.turn(&dt);
 
-                if n > 0 {
-                    if n >= dial {
-                        if dial != 0 {
-                            println!("click");
-                            clicks += 1;
-                        }
-                        dial += 100;
-                    }
-                    dial = (dial - n) % 100;
-                }
-            }
-            Some(D1Instruction::Right(mut n)) => {
-                while n >= 100 {
-                    println!("click");
-                    clicks += 1;
-                    n -= 100;
-                }
-
-                if n > 0 {
-                    dial += n;
-                    if dial >= 100 {
-                    println!("click");
-                        clicks += 1;
-                        dial -= 100;
-                    }
-                }
-            }
-        }
     }
 
-    println!("dial:{}", dial);
-    clicks
+    zeroes
 }
 
 fn get_whole_file(path: &str) -> String {
@@ -107,9 +109,7 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_d1p1() {
-        let test_inp = "L68
+    const D1_TEST_INPUT: &'static str ="
 L30
 R48
 L5
@@ -120,27 +120,17 @@ L99
 R14
 L82
 ";
-        let expected = 3;
 
-        assert_eq!(expected, day1part1(test_inp));
+    #[test]
+    fn test_d1p1() {
+        let expected = 3;
+        assert_eq!(expected, day1part1(D1_TEST_INPUT));
     }
 
     #[test]
     fn test_d1p2() {
-         let test_inp = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
-";
         let expected = 6;
-
-        assert_eq!(expected, day1part2(test_inp));
+        assert_eq!(expected, day1part2(D1_TEST_INPUT));
     }
 }
 
